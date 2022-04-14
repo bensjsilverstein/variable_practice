ef1f9043e73fd3b549e0c854eaeff39f27b60ca6
Ben Silverstein <benjsilverstein@comcast.net>
2022-04-13 20:20:50 -0600

    Annotate variables

--- variable.rb
+++ variable.rb
@@ -1,14 +1,18 @@
+#Added 3 variables and assigned them to strings
 first_name = "Ben"
 middle_initial = "J"
 last_name = "Silverstein"
+#Added 3 variables and assigned them to integers
 age = 29
 pets = 2
 siblings = 1
+#Added 2 variables and assigned them to floats
 time_studying = 20.5
 desk_change = 3.50
+# Added 2 variables and assigned them to booleans
 laundry_done = true
 trash_removed = true
-
+# Reassigned 3 variables
 first_name = "Benjamin"
 pets = 0
 trash_removed = false
